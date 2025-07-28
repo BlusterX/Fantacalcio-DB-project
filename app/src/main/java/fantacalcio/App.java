9fba1467b9f3528e18ee1c2a6da28aca344f8356
Gjovalin Mujaj <gjovalin.mujaj@studio.unibo.it>
2025-07-28 18:57:01 +0200

    Created project configuration files

---
+++ app/src/main/java/fantacalcio/App.java
@@ -0,0 +1,8 @@
+package fantacalcio;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the FantaCalcio application!");
+        // Additional initialization code can go here
+    }
+}
